5c75d5b447eb1ecf52897476cc6c58a4da6c70c7
Tonycons-dev <70544875+Tonycons-dev@users.noreply.github.com>
2024-09-25 19:02:40 -0400

    Update profile.cmake

--- src/arch/i386/profile.cmake
+++ src/arch/i386/profile.cmake
@@ -1,14 +1,17 @@
 function(LOAD_ARCH_PROFILE)
 
+    # Set this to whatever folder contains your i686-elf-gcc compiler (end with slash)
+    set(ARCH_COMPILER_PATH "")
+
     set(ARCH "i386" PARENT_SCOPE)
     set(ARCH_DEFINITIONS "__ARCH_I386__" PARENT_SCOPE)
-    set(ARCH_C_COMPILER "/home/anthony/cross/i686-elf-tools/bin/i686-elf-gcc" PARENT_SCOPE)
+    set(ARCH_C_COMPILER ${ARCH_COMPILER_PATH}i686-elf-gcc PARENT_SCOPE)
     set(ARCH_C_FLAGS "-m32 -ffreestanding -O2 -nostdlib" PARENT_SCOPE)
-    set(ARCH_CXX_COMPILER "/home/anthony/cross/i686-elf-tools/bin/i686-elf-g++" PARENT_SCOPE)
-    set(ARCH_LINKER "/home/anthony/cross/i686-elf-tools/bin/i686-elf-ld" PARENT_SCOPE)
+    set(ARCH_CXX_COMPILER ${ARCH_COMPILER_PATH}i686-elf-g++ PARENT_SCOPE)
+    set(ARCH_LINKER ${ARCH_COMPILER_PATH}i686-elf-ld PARENT_SCOPE)
     set(ARCH_ASSEMBLER "ASM_NASM" PARENT_SCOPE)
     set(ARCH_LINKER_LAYOUT "src/arch/i386/linker.ld" PARENT_SCOPE)
-    set(ARCH_LINKER_FLAGS "-m elf_i386 -T/home/anthony/Desktop/os/src/arch/i386/linker.ld -nostdlib" PARENT_SCOPE)
+    set(ARCH_LINKER_FLAGS "-m elf_i386 -T${CMAKE_CURRENT_LIST_DIR}/linker.ld -nostdlib" PARENT_SCOPE)
 
     set(I386_BOOT_ASM "src/arch/i386/boot/boot.asm")
     file(GLOB I386_MAIN_SRC "src/arch/i386/main/*.c")
